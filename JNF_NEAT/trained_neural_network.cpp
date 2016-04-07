f2d0b041d530eaee6a4aab6d9986e95dbedbfbfb
Jan Nils Ferner <jn_ferner@hotmail.de>
2016-04-07 16:06:44 +0200

    Actually included the empty class mentioned in the last commit

---
+++ JNF_NEAT/trained_neural_network.cpp
@@ -0,0 +1,18 @@
+#include "trained_neural_network.h"
+
+TrainedNeuralNetwork::TrainedNeuralNetwork(const NeuralNetwork & trainedNetwork):
+	trainedNetwork(trainedNetwork)
+{
+}
+
+void TrainedNeuralNetwork::LoadFromFile(std::string fileName)
+{
+	// TODO jnf
+	// Implementation
+}
+
+void TrainedNeuralNetwork::SaveToFile(std::string fileName) const
+{
+	// TODO jnf
+	// Implementation
+}
