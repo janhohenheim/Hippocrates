a3beed83309af5ee7fb95190851a40856c0745ca
Jan Nils Ferner <jn_ferner@hotmail.de>
2016-09-08 23:43:01 +0200

    Unify indentation

--- ConvolutionalNeuralNetwork/neuron.h
+++ ConvolutionalNeuralNetwork/neuron.h
@@ -10,18 +10,18 @@ class Neuron : public ISubSampler {
 public:
 	Neuron(double bias, std::vector<double>&& weights) : bias(bias), weights(std::move(weights)) {}
 
-    auto ProcessMatrix(Matrix::Position position, const Matrix& matrix) const -> Matrix override;
+	auto ProcessMatrix(Matrix::Position position, const Matrix& matrix) const -> Matrix override;
 	auto GetReceptiveField() const noexcept -> Matrix::Size override;
 	auto GetZeroPadding() const noexcept -> Matrix::Size override;
 
-    auto GetBias() const noexcept { return bias; }
-    auto GetWeights() const noexcept { return weights; }
+	auto GetBias() const noexcept { return bias; }
+	auto GetWeights() const noexcept { return weights; }
 
 private:
-    static auto sigmoid(Matrix::elementType n) -> double;
-    double bias = 0;
-    std::vector<double> weights;
-    Matrix::Size receptiveField;
+	static auto sigmoid(Matrix::elementType n) -> double;
+	double bias = 0;
+	std::vector<double> weights;
+	Matrix::Size receptiveField;
 };
 
 }
