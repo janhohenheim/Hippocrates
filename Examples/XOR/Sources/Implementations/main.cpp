05aafc5f349e783f39cadc51c230de3856a77981
Jeremy Stucki <stucki.jeremy@gmail.com>
2016-09-19 23:21:01 +0200

    Clean xor_example

--- Examples/XOR/Sources/Implementations/main.cpp
+++ Examples/XOR/Sources/Implementations/main.cpp
@@ -1,81 +1,59 @@
 #include <iostream>
-#include <time.h>
 #include <fstream>
 #include <chrono>
 
-// TODO: Replace with library imports
+// TODO: Replace with library import
 #include "../../../../Core/Sources/Headers/neural_network_trainer.h"
-#include "../../../../Core/Sources/Headers/training_data.h"
 
 using namespace std;
 using namespace JNF_NEAT;
 
-template<typename TimeT = std::chrono::milliseconds>
-struct measure {
-	template<typename F, typename ...Args>
-	static typename TimeT::rep execution(F&& func, Args&&... args)
-	{
-		auto start = std::chrono::steady_clock::now();
-		std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
-		auto duration = std::chrono::duration_cast< TimeT>
-			(std::chrono::steady_clock::now() - start);
-		return duration.count();
-	}
+enum class XORResult {
+	Zero,
+	One,
+	ClassificationCount
 };
 
+auto TestNetwork(NeuralNetwork &network, TrainingData<std::vector<float>, XORResult> &data) {
+	int errorCount = 0;
 
-auto TestXOR(TrainedNeuralNetwork& net) {
-	auto errorCount = 0;
-	auto XOR = [&](const std::initializer_list<float>& in) {
-		auto result = net.GetOutputsUsingInputs(in);
-		return result.back() > result.front();
-	};
-	auto TestXOR = [&](const std::initializer_list<float>& in, auto expectedResult) {
-		cout << "Testing with inputs ";
-		for (const auto& i: in) {
-			cout << i << ' ';
-		}
-		cout << "\nExpected " << expectedResult << ", got ";
-		auto result = XOR(in);
-		cout << result << " (";
-		if (result == expectedResult) {
-			cout << "success";
-		} else {
-			cout << "fail"; 
+	for (const auto& dataSet: data) {
+		auto networkOutputs = network.GetOutputsUsingInputs(dataSet.input);
+
+		if (networkOutputs[0] < networkOutputs[1] != static_cast<size_t>(dataSet.classification)) {
+			std::cout << "Network returned " << networkOutputs[0] << "/" << networkOutputs[1] << " "
+					  << "for inputs "       << dataSet.input[0]  << "/" << dataSet.input[1]  << ".";
 			errorCount++;
 		}
-		cout << ")\n\n";
-	};
-
-	TestXOR({ 0.0f, 0.0f }, 0);
-	TestXOR({ 0.0f, 1.0f }, 1);
-	TestXOR({ 1.0f, 0.0f }, 1);
-	TestXOR({ 1.0f, 1.0f }, 0);
-
-	cout << "Tests finished with " << errorCount << " errors\n\n";
+	}
 
 	return errorCount;
 }
 
+auto SaveNetwork(TrainedNeuralNetwork &champ, string filename) {
+	ofstream outFile(filename);
+	champ.SaveToFile(outFile);
+	outFile.close();
+}
+
+auto LoadNetwork(string filename) {
+	ifstream inFile(filename);
+	auto champ = TrainedNeuralNetwork::LoadFromFile(inFile);
+	inFile.close();
+
+	return champ;
+}
+
 int main() {
-	// Initilize Random Generator
 	srand(static_cast<unsigned>(time(nullptr)));
 
-	enum class XORResults {
-		Zero,
-		One,
-		ClassificationCount
-	};
-	TrainingData<std::vector<float>, XORResults> data;
-	data.AddSet({ { 0.0f, 0.0f }, XORResults::Zero });
-	data.AddSet({ { 0.0f, 1.0f }, XORResults::One });
-	data.AddSet({ { 1.0f, 0.0f }, XORResults::One });
-	data.AddSet({ { 1.0f, 1.0f }, XORResults::Zero });
+	TrainingData<std::vector<float>, XORResult> data;
+	data.AddSet({ {0.0f, 0.0f}, XORResult::Zero });
+	data.AddSet({ {0.0f, 1.0f}, XORResult::One });
+	data.AddSet({ {1.0f, 0.0f}, XORResult::One });
+	data.AddSet({ {1.0f, 1.0f}, XORResult::Zero });
 
-	// Training
 	NeuralNetworkTrainer trainer;
-
-	// Enable this if you want to fill your SSD with a gigabyte of JSON
 	trainer.loggingEnabled = false;
 
 	#ifdef CI
@@ -83,27 +61,9 @@ int main() {
 	#endif
 
 	auto champ = trainer.TrainSupervised(data, 50);
-	cout << "XOR outputs after training\n";
-	
-	int errorCount = TestXOR(champ);
 
-	string filename = "champ.nn";
+	SaveNetwork(champ, "champ.nn");
+	LoadNetwork("champ.nn");
 
-	// Saving
-	ofstream outFile(filename);
-	champ.SaveToFile(outFile);
-	outFile.close();
-
-	//cout << "XOR outputs after saving\n";
-	//GetXOROutputs(champ);
-
-	// Loading
-	ifstream inFile(filename);
-	champ = TrainedNeuralNetwork::LoadFromFile(inFile);
-	inFile.close();
-
-	//cout << "XOR outputs after loading\n";
-	//GetXOROutputs(champ);
-
-	return errorCount;
+	return TestNetwork(champ, data);
 }
