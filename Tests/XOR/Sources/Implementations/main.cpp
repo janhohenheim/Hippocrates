8a36f23612655e7004867ed86fd088a6801ccfe0
Jan Nils Ferner <jn_ferner@hotmail.de>
2016-09-21 20:33:58 +0200

    Restructure tests

--- Tests/XOR/Sources/Implementations/main.cpp
+++ Tests/XOR/Sources/Implementations/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <fstream>
 #include <chrono>
 
 // TODO: Replace with library import
-#include "../../../../Core/Sources/Headers/neural_network_trainer.h"
+#include "network_tester.h"
+
 
 using namespace std;
 using namespace JNF_NEAT;
@@ -14,33 +14,7 @@ enum class XORResult {
 	ClassificationCount
 };
 
-template <typename InputType, typename Classification>
-auto TestNetwork(NeuralNetwork &network, TrainingData<InputType, Classification> &data) {
-	int errorCount = 0;
 
-	for (const auto& dataSet: data) {
-		auto networkOutputs = network.GetOutputsUsingInputs(dataSet.input);
-
-		auto maxOutput = std::max_element(networkOutputs.begin(), networkOutputs.end());
-		auto outputIndex = std::distance(networkOutputs.begin(), maxOutput);
-
-		if (outputIndex != static_cast<size_t>(dataSet.classification)) {
-			std::cout << "Incorrect classification for inputs:";
-			for (auto& input: dataSet.input) {
-				std::cout << " - " << input << std::endl;
-			}
-
-			std::cout << "Got outputs:";
-			for (auto& output: networkOutputs) {
-				std::cout << " - " << output << std::endl;
-			}
-
-			std::cout << std::endl;
-		}
-	}
-
-	return errorCount;
-}
 
 auto SaveNetwork(TrainedNeuralNetwork &champ, string filename) {
 	ofstream outFile(filename);
@@ -77,5 +51,5 @@ int main() {
 	SaveNetwork(champ, "champ.nn");
 	LoadNetwork("champ.nn");
 
-	return TestNetwork(champ, data);
+	return Tests::TestingUtilities::TestNetwork(champ, data);
 }
