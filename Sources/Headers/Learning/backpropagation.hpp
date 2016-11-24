8f552f8269f496e7fe489d30324724ba60590d40
jan Nils Ferner <jn_ferner@hotmail.de>
2016-11-24 16:20:55 +0100

    GCC plz

--- Sources/Headers/Learning/backpropagation.hpp
+++ Sources/Headers/Learning/backpropagation.hpp
@@ -6,11 +6,12 @@ namespace Convolutional::Learning {
 	template<typename Classification>
 	class Backpropagation : public ILearningMethod<Classification> {
 	public:
-		using ILearningMethod::ILearningMethod;
+		using parent_t = ILearningMethod<Classification>;
+		using parent_t::ILearningMethod;
 
-		auto BeginEpoch(typename networks_t::iterator network) -> void override {}
-		auto EvaluateSet(typename networks_t::iterator network, typename TrainingData<Classification>::const_iterator set) -> void override {}
-		auto EndEpoch(typename networks_t::iterator network) -> void override {}
+		auto BeginEpoch(typename parent_t::networks_t::iterator network) -> void override {}
+		auto EvaluateSet(typename parent_t::networks_t::iterator network, typename TrainingData<Classification>::const_iterator set) -> void override {}
+		auto EndEpoch(typename parent_t::networks_t::iterator network) -> void override {}
 	};
 
 }
