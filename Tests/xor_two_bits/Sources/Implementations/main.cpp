5d36e29a535b64537ac97cbbeb161fa032340e15
jan Nils Ferner <jn_ferner@hotmail.de>
2016-12-01 10:33:38 +0100

    Clean tests up, fix xor_three_bits not being tested correctly

--- Tests/xor_two_bits/Sources/Implementations/main.cpp
+++ Tests/xor_two_bits/Sources/Implementations/main.cpp
@@ -20,19 +20,5 @@ int main() {
 	std::chrono::seconds timeout(10);
 	auto champ = Tests::TestingUtilities::TrainWithTimeout(trainer, data, timeout);
 	std::cout << "Finished training in " << trainer.GetGenerationsPassed() << " generations\n";
-
-	Training::Data<XORResult> expectedData;
-	expectedData.AddSet( { 1, 0 }, XORResult::One );
-	expectedData.AddSet( { 1, 1 }, XORResult::Zero );
-	expectedData.AddSet( { 1, 1 }, XORResult::Zero );
-	expectedData.AddSet( { 1, 1 }, XORResult::Zero );
-	expectedData.AddSet( { 0, 1 }, XORResult::One );
-	expectedData.AddSet( { 0, 0 }, XORResult::Zero );
-	expectedData.AddSet( { 0, 0 }, XORResult::Zero );
-	expectedData.AddSet( { 0, 1 }, XORResult::One );
-	expectedData.AddSet( { 0, 0 }, XORResult::Zero );
-	expectedData.AddSet( { 1, 1 }, XORResult::Zero );
-
-
-	return Tests::TestingUtilities::TestNetwork(champ, expectedData);
+	return Tests::TestingUtilities::TestNetwork(champ, data);
 }
