5f6d7356c9e04a6cc576518d48a6c7577e90a57f
jan Nils Ferner <jn_ferner@hotmail.de>
2016-12-05 17:23:12 +0100

    Change unsupervised learning to use more exact fitness function

--- Core/Sources/Headers/body/supervised_learning.hpp
+++ Core/Sources/Headers/body/supervised_learning.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <algorithm>
+#include <cmath>
+#include <iostream>
 
 #include "body.hpp"
 #include "training/data.hpp"
@@ -12,15 +14,13 @@ public:
 	using Data = Training::Data<Classification, ClassificationCount>;
 
 	explicit SupervisedLearning(const Data& data) :
-			trainingData { data },
-			inputCount{data[0].input.size()},
-			outputCount{ClassificationCount},
-			maxFitness{static_cast<Type::fitness_t>(data.GetSize())}
+			trainingData{ data },
+			inputCount{ data[0].input.size() }
 	{};
 
-	auto Reset() -> void override { currSetIndex = 0; fitness = 0.0; };
+	auto Reset() -> void override { currSetIndex = 0; error = 0; };
 	auto Update(const Type::neuron_values_t& networkOutputs) -> void override;
-	auto GetFitness() const -> Type::fitness_t override { return fitness; }
+	auto GetFitness() const -> Type::fitness_t override { return maxError - error; }
 
 	auto HasFinishedTask() const -> bool override { return currSetIndex == trainingData.GetSize(); };
 	auto ProvideNetworkWithInputs() const-> Type::neuron_values_t override {
@@ -32,22 +32,30 @@ public:
 	auto GetInputCount() const -> std::size_t override { return inputCount; };
 	auto GetOutputCount() const -> std::size_t override { return outputCount; };
 
-	auto GetMaximumFitness() const -> Type::fitness_t override { return maxFitness; };
+	auto GetMaximumFitness() const -> Type::fitness_t override { return maxError - 0; };
 
 private:
 	const Data& trainingData;
-	std::size_t currSetIndex = 0;
-	Type::fitness_t fitness = static_cast<Type::fitness_t>(0.0);
 	const std::size_t inputCount;
-	const std::size_t outputCount;
-	const Type::fitness_t maxFitness;
+	const std::size_t outputCount = ClassificationCount;
+	const Type::fitness_t maxError = trainingData.GetSize() * outputCount * std::abs(GetMaxActivation() - GetMinActivation());
+
+	Type::fitness_t error = 0;
+	std::size_t currSetIndex = 0;
+
+	auto GetMinActivation() const { return Type::neuron_value_t(-1); }
+	auto GetMaxActivation() const { return Type::neuron_value_t(+1); }
 };
 
 template <typename Classification, std::size_t ClassificationCount>
 auto SupervisedLearning<Classification, ClassificationCount>::Update(const Type::neuron_values_t& networkOutputs) -> void {
-	auto maxOutput = std::distance(networkOutputs.begin(), std::max_element(networkOutputs.begin(), networkOutputs.end()));
-	if (static_cast<size_t>(maxOutput) == static_cast<size_t>(trainingData[currSetIndex].classification)) {
-		fitness += static_cast<Type::fitness_t>(1.0);
+	for (std::size_t i = 0; i < networkOutputs.size(); ++i) {
+		const auto classificationAsNr = static_cast<size_t>(trainingData[currSetIndex].classification);
+		if (classificationAsNr == i) {
+			error += std::abs(networkOutputs[i] - GetMaxActivation());
+		} else {
+			error += std::abs(networkOutputs[i] - GetMinActivation());
+		}
 	}
 	++currSetIndex;
 }
