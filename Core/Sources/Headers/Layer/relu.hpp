427bfeb7f0e6420aae561571eb84157b7fbcc1b7
jan Nils Ferner <jn_ferner@hotmail.de>
2016-12-15 18:07:22 +0100

    Add ReLU layer

---
+++ Core/Sources/Headers/Layer/relu.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "ilayer.hpp"
+
+namespace Convolutional::Layer {
+class ReLU : public ILayer {
+public:
+	using ILayer::ILayer;
+	auto ProcessMultiMatrix(const MultiMatrix& multiMatrix) -> MultiMatrix override;
+
+	auto GetReceptiveField(Matrix::Size size) const noexcept -> Matrix::Size override { return size; }
+	auto GetZeroPadding(Matrix::Size size) const noexcept -> Matrix::Size override { return {0, 0}; }
+	auto GetStride(Matrix::Size size) const noexcept -> Matrix::Size override { return {1, 1}; }
+	
+	auto Clone() const noexcept->std::unique_ptr<ILayer> override { return std::make_unique<ReLU>(*this); }
+};
+
+}
---
+++ Core/Sources/Implementations/Layer/relu.cpp
@@ -0,0 +1,13 @@
+﻿#include "Layer/relu.hpp"
+#include <algorithm>
+
+using namespace Convolutional;
+using namespace Convolutional::Layer;
+
+auto ReLU::ProcessMultiMatrix(const MultiMatrix & multiMatrix) -> MultiMatrix {
+	auto rectifiedMultiMatrix = multiMatrix;
+	for (auto& matrix : rectifiedMultiMatrix)
+		for (auto& element : matrix)
+			element = std::max(static_cast<Matrix::element_t>(0), element);
+	return rectifiedMultiMatrix;
+}
