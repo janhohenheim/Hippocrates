dc62a940e2188b7a9a4c26a1535b04df171ea499
Jeremy Stucki <stucki.jeremy@gmail.com>
2016-12-24 15:02:04 +0100

    Add module "image_recognition"

---
+++ hippocrates/neat/modules/image_recognition/source/implementation/main.cpp
@@ -0,0 +1,11 @@
+#include "neural_network_trainer.hpp"
+#include "fully_connected_neural_network.hpp"
+
+using namespace Hippocrates;
+
+int main() {
+	Training::NeuralNetworkTrainer trainer;
+	Convolutional::Layer::FullyConnectedNeuralNetwork(10);
+
+	return 0;
+}
