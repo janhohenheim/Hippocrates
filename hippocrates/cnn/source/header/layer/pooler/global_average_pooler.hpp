c10488df1d4236763eb35475057a46e5c62d3d74
Mafii <mafi@fog.im>
2017-01-03 19:47:45 +0100

    CNN: Add GlobalAveragePooler
    Fix VS Config
    GlobalAveragePooler is currently a pure copy of maxpooler
    to be implemented later on.

---
+++ hippocrates/cnn/source/header/layer/pooler/global_average_pooler.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "layer/pooler/ipooler.hpp"
+
+namespace Convolutional::Layer::Pooler {
+
+	class GlobalAveragePooler : public IPooler {
+	public:
+		using IPooler::IPooler;
+
+		GlobalAveragePooler(Matrix::Size receptiveField = { 2, 2 },
+			Matrix::Size stride = { 2, 2 })
+			: receptiveField(receptiveField),
+			stride(stride)
+		{ }
+		GlobalAveragePooler(const GlobalAveragePooler&) = default;
+		GlobalAveragePooler(GlobalAveragePooler&&) = default;
+
+		auto ProcessMultiMatrix(const MultiMatrix& multiMatrix)->MultiMatrix override;
+
+		auto GetReceptiveField(Matrix::Size size) const noexcept -> Matrix::Size override { return receptiveField; }
+		auto GetZeroPadding(Matrix::Size size) const noexcept -> Matrix::Size override { return { 0, 0 }; }
+		auto GetStride(Matrix::Size size) const noexcept -> Matrix::Size override { return stride; }
+
+		auto Clone() const noexcept -> std::unique_ptr<ILayer> override { return std::make_unique<GlobalAveragePooler>(*this); }
+
+
+		auto ProcessMatrix(const Matrix& matrix) const->Matrix::element_t;
+		auto GetSizeAfterPooling(Matrix::Size originalSize) const->Matrix::Size;
+
+	private:
+		Matrix::Size receptiveField;
+		Matrix::Size stride;
+	};
+
+}
