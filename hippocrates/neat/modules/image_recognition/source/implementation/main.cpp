dbb718de59eacc87b271ca51311f13b2c5f486e6
jan Nils Ferner <jn_ferner@hotmail.de>
2017-01-04 20:27:03 +0100

    NEAT: Fix... something?

--- hippocrates/neat/modules/image_recognition/source/implementation/main.cpp
+++ hippocrates/neat/modules/image_recognition/source/implementation/main.cpp
@@ -1,11 +1,11 @@
 #include "neural_network_trainer.hpp"
-#include "fully_connected_neural_network.hpp"
+#include "fully_connected.hpp"
 
 using namespace Hippocrates;
 
 int main() {
 	Training::NeuralNetworkTrainer trainer;
-	Convolutional::Layer::FullyConnectedNeuralNetwork(10);
+	Convolutional::Layer::FullyConnected(10);
 
 	return 0;
 }
