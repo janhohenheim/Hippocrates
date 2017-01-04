3eb0130541cb704c86a9e61e217f0c34d40b6c7f
jan Nils Ferner <jn_ferner@hotmail.de>
2017-01-04 17:08:21 +0100

    Add forgotten files

---
+++ hippocrates/cnn/source/implementation/layer/residual_block.cpp
@@ -0,0 +1,16 @@
+﻿#include "layer/residual_block.hpp"
+
+using namespace Convolutional;
+using namespace Convolutional::Layer;
+
+auto ResidualBlock::ProcessMultiMatrix(const MultiMatrix & multiMatrix) -> MultiMatrix {
+	return multiMatrix;
+}
+
+auto ResidualBlock::GetSizeAfterProcessing() const noexcept -> Matrix::Size {
+	return Matrix::Size();
+}
+
+auto ResidualBlock::GetElementCountAfterProcessing() const noexcept -> std::size_t {
+	return std::size_t();
+}
