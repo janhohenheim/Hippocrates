da9a560e9b141339acc23e69f04efb44f932bc42
jan Nils Ferner <jn_ferner@hotmail.de>
2017-01-09 11:43:58 +0100

    CNN: Update ImageMagick version

--- ci/install_dependencies.ps1
+++ ci/install_dependencies.ps1
@@ -9,7 +9,7 @@ Tasks=modifypath,install_devel
 $tmp = "$env:TMP\$([System.Guid]::NewGuid())"
 
 $configFile = "$tmp\ImageMagick.cfg"
-$downloadUrl = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.7-2-Q8-x64-dll.exe'
+$downloadUrl = 'https://www.imagemagick.org/download/binaries/ImageMagick-6.9.7-3-Q8-x64-dll.exe'
 $installer = "$tmp\imagemagick-installer.exe"
 
 if (Test-Path "C:\Program Files\ImageMagick") {
